ad3fd0a9a4c772ef64bd4da89a4566c3965079bd
zhangmo <1258223683@qq.com>
2017-08-02 18:54:40 +0800

    which one is right

--- travelsuper/travel-jar-common/src/main/java/com/travel/test/Test1.java
+++ travelsuper/travel-jar-common/src/main/java/com/travel/test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("我第一次用git");
+		System.out.println("which on is right");
 	}
 }

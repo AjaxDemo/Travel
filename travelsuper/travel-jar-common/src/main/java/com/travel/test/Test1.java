d96886f14175f02c77509044275b683810b35524
AjaxDemo <13472249721@163.com>
2017-08-03 21:52:33 +0800

    test1修改编码

--- travelsuper/travel-jar-common/src/main/java/com/travel/test/Test1.java
+++ travelsuper/travel-jar-common/src/main/java/com/travel/test/Test1.java
@@ -3,8 +3,8 @@ package com.travel.test;
 public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("我第一次用git");
+		System.out.println("淇敼缂栫爜git");
 		System.out.println("which on is right");
-		System.out.println("tra2 添加修改");
+		System.out.println("tra2淇敼缂栫爜");
 	}
 }
